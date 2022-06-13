480a0d6382290abd962d9455085706d66b51a377
Evan Mattiza <emattiza@gmail.com>
2022-06-13 14:27:38 +0000

    added basic private function, and test placeholder

--- src/cpio.rs
+++ src/cpio.rs
@@ -1,11 +1,42 @@
-use std::convert::TryInto;
-use std::ffi::OsString;
+use std::ffi::{OsStr, OsString};
 use std::os::unix::ffi::OsStrExt;
+use std::os::unix::fs::MetadataExt;
 use std::path::PathBuf;
+use std::{convert::TryInto, fs::File};
 
+use cpio::{newc, write_cpio};
 use lazy_static::lazy_static;
+use walkdir::WalkDir;
 
 use crate::files::basename;
+
+fn make_archive_from_dir(path: PathBuf, out_path: &OsStr) -> std::io::Result<()> {
+    let dir = WalkDir::new(path)
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| {
+            let meta = entry.metadata();
+            meta.map(|x| x.is_file()).unwrap_or(false)
+        })
+        .filter_map(|entry| {
+            let entry_name = entry.path().to_str()?;
+            let meta = entry.metadata().ok()?;
+            let built = newc::Builder::new(entry_name)
+                .dev_major(meta.dev().try_into().ok()?)
+                .rdev_major(meta.rdev().try_into().ok()?)
+                .gid(meta.gid())
+                .uid(meta.uid())
+                .ino(meta.ino().try_into().ok()?)
+                .nlink(meta.nlink().try_into().ok()?)
+                .mode(meta.mode())
+                .mtime(meta.mtime().try_into().ok()?);
+            let readable_file = File::open(entry.path()).ok()?;
+            Some((built, readable_file))
+        });
+    let out_archive = File::create(out_path)?;
+    write_cpio(dir, out_archive)?;
+    Ok(())
+}
 
 fn make_leader_cpio() -> std::io::Result<Vec<u8>> {
     let mut leader_cpio = std::io::Cursor::new(vec![]);
@@ -94,4 +125,27 @@ lazy_static! {
         .len()
         .try_into()
         .expect("Failed to convert usize leader length to u64");
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{error::Error, io::Write};
+    use tempfile::{tempfile, NamedTempFile};
+
+    use super::make_archive_from_dir;
+
+    #[test]
+    fn test_single_file_archive() -> Result<(), Box<dyn Error>> {
+        let mut file = NamedTempFile::new()?;
+        let archive = NamedTempFile::new()?;
+        write!(file, "Hello cpio!")?;
+        make_archive_from_dir(file.path().to_path_buf(), archive.path().as_os_str())?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_multiple_file_archive() {}
+
+    #[test]
+    fn test_multiple_nested_file_archive() {}
 }
